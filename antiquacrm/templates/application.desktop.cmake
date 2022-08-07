27d4873f78b1b9cf05811e985f78bb4a21ece967
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2022-08-07 03:29:00 +0200

    So 7. Aug 06:54:07 CEST 2022

--- antiquacrm/templates/application.desktop.cmake
+++ antiquacrm/templates/application.desktop.cmake
@@ -5,6 +5,6 @@ Icon=@PROJECTNAME@
 Terminal=false
 GenericName=Antiquarian Book- and Ordermanager
 GenericName[de]=Antiquarischer Buch- und Bestellmanager
-Name=@PROJECTNAME@
+Name=@DISPLAYNAME@
 Comment=@DESCRIPTION@
 Categories=Utility;
