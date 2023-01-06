77dd8d15ac81c671dc4a60ec787f7dbcdf8c48c1
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2023-01-06 11:40:00 +0100

    Fr 6. Jan 13:25:17 CET 2023

---
+++ v0/scripts/scriptsconfig.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8
+
+_PROJECT_TARGET=$HOME/Developement/antiqua
+_PROJECT_SOURCE=$HOME/Developement/antiqua/v0/src
+
+_QT5_HOME=$HOME/Programme/Qt/5.15.2/gcc_64
+_QT5_TOOLS_HOME="$HOME/Programme/Qt/Tools"
+_QT5_CREATOR_HOME="${_QT5_TOOLS_HOME}/QtCreator"
+
+_INSTALL_DEST_DIR=$HOME/Downloads/AntiquaCRM_BUILD
