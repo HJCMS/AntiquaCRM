6a1e6fafd001a84d162582c5af5ff6ad283a5919
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2023-02-28 08:10:00 +0100

    Di 28. Feb 09:01:08 CET 2023

--- application/xdg/antiquacrm.iss.cmake
+++ application/xdg/antiquacrm.iss.cmake
@@ -1,69 +1,64 @@
-; -*- coding: utf-8 -*-
-; vim: set fileencoding=utf-8
+;; WARNING: ASCII with CRLF line terminators
 
-; Project main settings
-#define APPL_NAME "@PROJECTNAME@"
-#define APPL_UUID "@ANTIQUACRM_APPID@"
-#define APPL_DISPLAY_NAME "@DISPLAYNAME@"
-#define APPL_VERSION_STRING "@ANTIQUACRM_VERSION_MAJOR@.@ANTIQUACRM_VERSION_MINOR@.@ANTIQUACRM_VERSION_RELEASE@"
-#define APPL_PUBLISHER "HJCMS"
-#define APPL_PUBLISHER_URL "@HOMEPAGEURL@"
-#define APPL_BINARY "@PROJECTNAME@.exe"
-#define APPL_DOMAIN "hjcms.de"
-#define APPL_BUILD_YEAR GetDateTimeString('yyyy', '', '')
-#define APPL_BUILD_DIR "@ANTIQUACRM_INNOSETUP_BINDIR@"
-#define APPL_SOURCE_DIR "@ANTIQUACRM_INNOSETUP_SRCDIR@"
-#define APPL_TEMPLATE_DIR "@CMAKE_CURRENT_APPL_SOURCE_DIR@"
-#define APPL_PACKAGE_RELEASE GetDateTimeString('yyyymmdd', '', '')
+;; Project main settings
+#define MY_APPL_NAME "@PROJECTNAME@"
+#define MY_APPL_UUID "@ANTIQUACRM_APPID@"
+#define MY_APPL_DISPLAY_NAME "@DISPLAYNAME@"
+#define MY_APPL_VERSION "@ANTIQUACRM_VERSION_MAJOR@.@ANTIQUACRM_VERSION_MINOR@.@ANTIQUACRM_VERSION_RELEASE@"
+#define MY_APPL_PUBLISHER "HJCMS"
+#define MY_APPL_HOMEPAGE "@HOMEPAGEURL@"
+#define MY_APPL_FAQ_URL "https://github.com/HJCMS/AntiquaCRM"
+#define MY_APPL_BINARY "@PROJECTNAME@.exe"
+#define MY_APPL_DOMAIN "hjcms.de"
 
-; Qt5 path
-#define APPL_QT_PATH "E:\Development\qt\5.15.2\mingw81_64"
+;; Release date and times
+#define CURRENT_YEAR GetDateTimeString('yyyy', '', '')
+#define MY_APPL_PKG_RELEASE GetDateTimeString('yyyy-mm-dd-hhMM', '', '')
 
-; QREncode
-#define APPL_QRENCODE_PATH "E:\Development\qt\5.15.2\mingw81_64"
-
-; PostgreSQL
-#define APPL_PGSQL_PATH ""
-
-; Personal template files target
-#define APPL_COPY_DIR ""
-
-; Required for cUrl CA_BUNDLE PEM File.
-; https://curl.se/docs/sslcerts.html
-; NOTE Make sure, the destination filename is equal to "curl-ca-bundle.crt"!
-#define APPL_CA_BUNDLE ""
+;; Path settings
+#define BUILD_DIR "@ANTIQUACRM_CURRENT_BINARY_DIR@"
+#define SOURCE_DIR "@ANTIQUACRM_CURRENT_SOURCE_DIR@"
+#define TPL_DIR "@ANTIQUACRM_CURRENT_SOURCE_DIR@\xdg"
+#define QT5_PATH "E:\Development\qt\5.15.2\mingw81_64"
+#define QRENCODE_PATH "E:\Development\qt\5.15.2\mingw81_64"
+#define PgSQL_PATH "E:\Development\PostgreSQL\bin"
+#define CURL_PATH "E:\Development\cUrl\bin"
+;; Special copy target
+#define COPY_DIR "..."
+#define CURL_HOME "..."
 
 [Setup]
-AppId={#APPL_UUID}
-AppName={#APPL_DISPLAY_NAME}
-AppVersion={#APPL_VERSION_STRING}
-AppVerName={#APPL_NAME} {#APPL_VERSION_STRING}
-AppPublisher={#APPL_PUBLISHER}
-AppPublisherURL={#APPL_PUBLISHER_URL}
-AppSupportURL={#APPL_PUBLISHER_URL}
-AppUpdatesURL={#APPL_PUBLISHER_URL}
-AppCopyright="Copyright(C) 2011-{#APPL_BUILD_YEAR} {#APPL_PUBLISHER} {#APPL_PUBLISHER_URL}"
-AppContact="{#APPL_PUBLISHER} {#APPL_PUBLISHER_URL}"
+AppId={#MY_APPL_UUID}
+AppName={#MY_APPL_DISPLAY_NAME}
+AppVersion={#MY_APPL_VERSION}
+AppVerName={#MY_APPL_NAME} {#MY_APPL_VERSION}
+AppPublisher={#MY_APPL_PUBLISHER}
+AppPublisherURL={#MY_APPL_HOMEPAGE}
+AppSupportURL={#MY_APPL_FAQ_URL}
+AppUpdatesURL={#MY_APPL_FAQ_URL}
+AppReadmeFile="{#MY_APPL_FAQ_URL}/blob/main/README.md"
+AppCopyright="Copyright(C) 2011-{#CURRENT_YEAR} {#MY_APPL_PUBLISHER} {#MY_APPL_HOMEPAGE}"
+AppContact="{#MY_APPL_PUBLISHER} {#MY_APPL_HOMEPAGE}"
 AppComments="Antiquarisches Datenbank Verwaltungsprogramm"
-DefaultDirName={commonpf64}\{#APPL_PUBLISHER}\{#APPL_NAME}
+DefaultGroupName={#MY_APPL_DISPLAY_NAME}
+DefaultDirName={commonpf64}\{#MY_APPL_PUBLISHER}\{#MY_APPL_NAME}
 DisableDirPage=yes
-DefaultGroupName={#APPL_NAME}
-LicenseFile={#APPL_TEMPLATE_DIR}\LICENSE.md
+LicenseFile={#TPL_DIR}\LICENSE.md
+OutputDir=.
 Compression=lzma
 SolidCompression=yes
 CreateUninstallRegKey=yes
 PrivilegesRequired=admin
 ArchitecturesAllowed=x64
-SetupIconFile={#APPL_TEMPLATE_DIR}\antiquacrm.ico
+SetupIconFile={#TPL_DIR}\antiquacrm.ico
 Uninstallable=yes
 UserInfoPage=no
 DirExistsWarning=yes
 UsePreviousAppDir=yes
+OutputBaseFilename={#MY_APPL_NAME}-{#MY_APPL_VERSION}-{#MY_APPL_PKG_RELEASE}-x86_64-installer
+;; Window Style
 WindowVisible=yes
 WindowShowCaption=no
-; Installer Output
-OutputDir=.
-OutputBaseFilename={#APPL_NAME}-{#APPL_VERSION_STRING}-x86_64-{#APPL_PACKAGE_RELEASE}-installer
 
 [Languages]
 Name: "english"; MessagesFile: "compiler:Default.isl"
@@ -74,19 +69,20 @@ english.NormalInst=Default Application Data
 german.NormalInst=Standard Programm Dateien
 
 [Messages]
-BeveledLabel=Copyright(C) 2011-2023 {#APPL_PUBLISHER} {#APPL_PUBLISHER_URL}
+BeveledLabel=Copyright(C) 2011-{#CURRENT_YEAR} {#MY_APPL_PUBLISHER} {#MY_APPL_HOMEPAGE}
 
-[Tasks] 
-Name: "application"; Description: "{#APPL_DISPLAY_NAME} {cm:CreateDesktopIcon}"; GroupDescription: "{cm:AdditionalIcons}"; Flags: unchecked
+[Tasks]
+Name: "application"; Description: "{#MY_APPL_DISPLAY_NAME} {cm:CreateDesktopIcon}"; GroupDescription: "{cm:AdditionalIcons}"; Flags: unchecked
 
 [Dirs]
+Name: "{app}"; Attribs: system;
 Name: "{app}\i18n"; Attribs: system;
 Name: "{app}\data"; Attribs: system;
 Name: "{app}\data\pgsql"; Attribs: system;
-Name: "{app}\data\json"; Attribs: system; 
+Name: "{app}\data\json"; Attribs: system;
 Name: "{app}\data\fonts"; Attribs: system;
 Name: "{app}\data\certs"; Attribs: system;
-Name: "{app}\plugins"; Attribs: system;  
+Name: "{app}\plugins"; Attribs: system;
 Name: "{app}\plugins\antiquacrm"; Attribs: system;
 Name: "{app}\plugins\antiquacrm\provider"; Attribs: system;
 Name: "{app}\plugins\imageformats"; Attribs: system;
@@ -102,75 +98,76 @@ Name: "{app}\plugins\printsupport"; Attribs: system;
 Name: "{app}\plugins\sqldrivers"; Attribs: system;
 
 [Files]
-Source: "{#APPL_BUILD_DIR}\src\antiquacrm.exe"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{#APPL_BUILD_DIR}\src\assistant\antiquacrm_assistant.exe"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{#APPL_BUILD_DIR}\src\lib\libAntiquaCRM.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{#APPL_TEMPLATE_DIR}\qt.conf"; DestDir: "{app}"; Flags: ignoreversion;
-Source: "{#APPL_BUILD_DIR}\src\i18n\antiquacrm_de.qm"; DestDir: "{app}\i18n"; Attribs: system; Flags: ignoreversion;
-Source: "{#APPL_BUILD_DIR}\src\data\pgsql\*.sql"; DestDir: "{app}\data\pgsql\"; Attribs: system; Flags: ignoreversion;
-Source: "{#APPL_BUILD_DIR}\src\data\json\*.json"; DestDir: "{app}\data\json\"; Attribs: system; Flags: ignoreversion;
-Source: "{#APPL_SOURCE_DIR}\src\data\fonts\*.ttf"; DestDir: "{app}\data\fonts"; Attribs: system; Flags: ignoreversion;
-Source: "{#APPL_BUILD_DIR}\src\plugins\libAntiquaCRMPlugin.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{#APPL_BUILD_DIR}\src\plugins\antiquacrm\abebooks\lib*.dll"; DestDir: "{app}\plugins\antiquacrm\provider"; Attribs: system; Flags: ignoreversion;
-Source: "{#APPL_BUILD_DIR}\src\plugins\antiquacrm\buchfreund\lib*.dll"; DestDir: "{app}\plugins\antiquacrm\provider"; Attribs: system; Flags: ignoreversion;
-Source: "{#APPL_BUILD_DIR}\src\plugins\antiquacrm\booklooker\lib*.dll"; DestDir: "{app}\plugins\antiquacrm\provider"; Attribs: system; Flags: ignoreversion;
-Source: "{#APPL_TEMPLATE_DIR}\*.png"; DestDir: "{app}\icons"; Flags: ignoreversion;
+Source: "{#BUILD_DIR}\src\antiquacrm.exe"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#TPL_DIR}\antiquacrm.ico"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#BUILD_DIR}\src\assistant\antiquacrm_assistant.exe"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#BUILD_DIR}\src\lib\libAntiquaCRM.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#TPL_DIR}\qt.conf"; DestDir: "{app}"; Flags: ignoreversion;
+Source: "{#BUILD_DIR}\src\i18n\antiquacrm_de.qm"; DestDir: "{app}\i18n"; Attribs: system; Flags: ignoreversion;
+Source: "{#BUILD_DIR}\src\data\pgsql\*.sql"; DestDir: "{app}\data\pgsql\"; Attribs: system; Flags: ignoreversion;
+Source: "{#BUILD_DIR}\src\data\json\*.json"; DestDir: "{app}\data\json\"; Attribs: system; Flags: ignoreversion;
+Source: "{#SOURCE_DIR}\src\data\fonts\*.ttf"; DestDir: "{app}\data\fonts"; Attribs: system; Flags: ignoreversion;
+Source: "{#BUILD_DIR}\src\plugins\libAntiquaCRMPlugin.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#BUILD_DIR}\src\plugins\antiquacrm\abebooks\lib*.dll"; DestDir: "{app}\plugins\antiquacrm\provider"; Attribs: system; Flags: ignoreversion;
+Source: "{#BUILD_DIR}\src\plugins\antiquacrm\buchfreund\lib*.dll"; DestDir: "{app}\plugins\antiquacrm\provider"; Attribs: system; Flags: ignoreversion;
+Source: "{#BUILD_DIR}\src\plugins\antiquacrm\booklooker\lib*.dll"; DestDir: "{app}\plugins\antiquacrm\provider"; Attribs: system; Flags: ignoreversion;
+Source: "{#TPL_DIR}\*.png"; DestDir: "{app}\icons"; Flags: ignoreversion;
 ;; MinGW
-Source: "{APPL_QT_PATH}\bin\libEGL.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\libgcc_s_seh-1.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\libstdc++-6.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\libwinpthread-1.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\libEGL.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\libgcc_s_seh-1.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\libstdc++-6.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\libwinpthread-1.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
 ;; PostgreSQL
-Source: "{APPL_PGSQL_PATH}\libcrypto-1_1-x64.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_PGSQL_PATH}\libiconv-2.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_PGSQL_PATH}\libintl-9.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_PGSQL_PATH}\liblz4.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_PGSQL_PATH}\libpq.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_PGSQL_PATH}\libssl-1_1-x64.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_PGSQL_PATH}\libwinpthread-1.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_PGSQL_PATH}\zlib1.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#PgSQL_PATH}\libcrypto-1_1-x64.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#PgSQL_PATH}\libiconv-2.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#PgSQL_PATH}\libintl-9.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#PgSQL_PATH}\liblz4.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#PgSQL_PATH}\libpq.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#PgSQL_PATH}\libssl-1_1-x64.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#PgSQL_PATH}\libwinpthread-1.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#PgSQL_PATH}\zlib1.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
 ;; QREncode
-Source: "{APPL_QRENCODE_PATH}\bin\libzlib1.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QRENCODE_PATH}\bin\libpng16.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QRENCODE_PATH}\lib\libqrencode.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QRENCODE_PATH}\bin\libzlib1.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QRENCODE_PATH}\bin\libpng16.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QRENCODE_PATH}\lib\libqrencode.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
 ;; Qt5
-Source: "{APPL_QT_PATH}\bin\Qt5Core.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\Qt5Gui.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\Qt5Widgets.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\Qt5Network.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\Qt5NetworkAuth.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\Qt5Sql.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\Qt5Svg.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\Qt5Xml.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\Qt5PrintSupport.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\Qt5Charts.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\bin\Qt5WinExtras.dll"; DestDir: "{app}"; Flags: ignoreversion;
-Source: "{APPL_QT_PATH}\plugins\generic\*.dll"; DestDir: "{app}\plugins\generic"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
-Source: "{APPL_QT_PATH}\plugins\imageformats\*.dll"; DestDir: "{app}\plugins\imageformats"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
-Source: "{APPL_QT_PATH}\plugins\iconengines\*.dll"; DestDir: "{app}\plugins\iconengines"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
-Source: "{APPL_QT_PATH}\plugins\platforms\*.dll"; DestDir: "{app}\plugins\platforms"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
-Source: "{APPL_QT_PATH}\plugins\platformthemes\*.dll"; DestDir: "{app}\plugins\platformthemes"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
-Source: "{APPL_QT_PATH}\plugins\printsupport\*.dll"; DestDir: "{app}\plugins\printsupport"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
-Source: "{APPL_QT_PATH}\plugins\sqldrivers\qsqlpsql.dll"; DestDir: "{app}\plugins\sqldrivers"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
+Source: "{#QT5_PATH}\bin\Qt5Core.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\Qt5Gui.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\Qt5Widgets.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\Qt5Network.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\Qt5NetworkAuth.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\Qt5Sql.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\Qt5Svg.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\Qt5Xml.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\Qt5PrintSupport.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\Qt5Charts.dll"; DestDir: "{app}"; Attribs: system; Flags: ignoreversion;
+Source: "{#QT5_PATH}\bin\Qt5WinExtras.dll"; DestDir: "{app}"; Flags: ignoreversion;
+Source: "{#QT5_PATH}\plugins\generic\*.dll"; DestDir: "{app}\plugins\generic"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
+Source: "{#QT5_PATH}\plugins\imageformats\*.dll"; DestDir: "{app}\plugins\imageformats"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
+Source: "{#QT5_PATH}\plugins\iconengines\*.dll"; DestDir: "{app}\plugins\iconengines"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
+Source: "{#QT5_PATH}\plugins\platforms\*.dll"; DestDir: "{app}\plugins\platforms"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
+Source: "{#QT5_PATH}\plugins\platformthemes\*.dll"; DestDir: "{app}\plugins\platformthemes"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
+Source: "{#QT5_PATH}\plugins\printsupport\*.dll"; DestDir: "{app}\plugins\printsupport"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
+Source: "{#QT5_PATH}\plugins\sqldrivers\qsqlpsql.dll"; DestDir: "{app}\plugins\sqldrivers"; Attribs: system; Flags: ignoreversion recursesubdirs createallsubdirs;
 ;; Kundendaten
-Source: "{APPL_COPY_DIR}\URWChancery_L-Medium-Italic.ttf"; DestDir: "{app}\company"; Flags: ignoreversion recursesubdirs createallsubdirs;
-Source: "{APPL_COPY_DIR}\druck_header.png"; DestDir: "{app}\company"; Flags: ignoreversion recursesubdirs createallsubdirs;
-Source: "{APPL_COPY_DIR}\Zertifikat\*.crt"; DestDir: "{app}\data\certs\"; Attribs: system; Flags: ignoreversion;
+Source: "{#COPY_DIR}\URWChancery_L-Medium-Italic.ttf"; DestDir: "{app}\company"; Flags: ignoreversion recursesubdirs createallsubdirs;
+Source: "{#COPY_DIR}\druck_header.png"; DestDir: "{app}\company"; Flags: ignoreversion recursesubdirs createallsubdirs;
+Source: "{#COPY_DIR}\Zertifikat\*.crt"; DestDir: "{app}\data\certs\"; Attribs: system; Flags: ignoreversion;
 ;; Curl CA-Bundle
-Source: "{#APPL_CA_BUNDLE}"; DestDir: "{app}\"; Attribs: system; Flags: ignoreversion;
+Source: "{#CURL_HOME}\curl-ca-bundle.crt"; DestDir: "{app}\"; Attribs: system; Flags: ignoreversion;
 
 [Icons]
-Name: "{group}\{#APPL_DISPLAY_NAME}"; Filename: "{app}\{#APPL_BINARY}";
-Name: "{group}\{cm:UninstallProgram,{#APPL_NAME}}"; Filename: "{uninstallexe}";
-Name: "{commondesktop}\{#APPL_DISPLAY_NAME}"; Filename: "{app}\{#APPL_BINARY}"; Tasks: "application";
+Name: "{group}\{#MY_APPL_DISPLAY_NAME}"; Filename: "{app}\{#MY_APPL_BINARY}"; IconFilename: "{app}\{#MY_APPL_NAME}.ico";
+Name: "{group}\{cm:UninstallProgram,{#MY_APPL_NAME}}"; Filename: "{uninstallexe}"; IconFilename: "{app}\{#MY_APPL_NAME}.ico";
+Name: "{commondesktop}\{#MY_APPL_DISPLAY_NAME}"; Filename: "{app}\{#MY_APPL_BINARY}"; Tasks: "application"; IconFilename: "{app}\{#MY_APPL_NAME}.ico";
 
 [Registry]
 ;; HKEY_LOCAL_MACHINE
-Root: HKLM; Subkey: "Software\HJCMS\{#APPL_BINARY}";
-Root: HKLM; Subkey: "Software\HJCMS\{#APPL_BINARY}"; ValueType: string; ValueName: "Version"; ValueData: "{#APPL_VERSION_STRING}"; Flags: uninsdeletekey;
-Root: HKLM; Subkey: "Software\HJCMS\{#APPL_BINARY}"; ValueType: string; ValueName: "Author"; ValueData: "{#APPL_PUBLISHER}"; Flags: uninsdeletekey;
-Root: HKLM; Subkey: "Software\HJCMS\{#APPL_BINARY}"; ValueType: string; ValueName: "Homepage"; ValueData: "{#APPL_PUBLISHER_URL}"; Flags: uninsdeletekey;
-Root: HKLM; Subkey: "Software\HJCMS\{#APPL_BINARY}"; ValueType: string; ValueName: "InstallPath"; ValueData: "{commonpf64}\HJCMS\{#APPL_BINARY}"; Flags: uninsdeletekey;
+Root: HKLM; Subkey: "Software\HJCMS\{#MY_APPL_BINARY}";
+Root: HKLM; Subkey: "Software\HJCMS\{#MY_APPL_BINARY}"; ValueType: string; ValueName: "Version"; ValueData: "{#MY_APPL_VERSION}"; Flags: uninsdeletekey;
+Root: HKLM; Subkey: "Software\HJCMS\{#MY_APPL_BINARY}"; ValueType: string; ValueName: "Author"; ValueData: "{#MY_APPL_PUBLISHER}"; Flags: uninsdeletekey;
+Root: HKLM; Subkey: "Software\HJCMS\{#MY_APPL_BINARY}"; ValueType: string; ValueName: "Homepage"; ValueData: "{#MY_APPL_HOMEPAGE}"; Flags: uninsdeletekey;
+Root: HKLM; Subkey: "Software\HJCMS\{#MY_APPL_BINARY}"; ValueType: string; ValueName: "InstallPath"; ValueData: "{commonpf64}\HJCMS\{#MY_APPL_BINARY}"; Flags: uninsdeletekey;
 
 ;; EOF
 
