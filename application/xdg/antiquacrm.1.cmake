81b1ad02c5e231cd3d417e617a43ba936e1a31e3
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2023-03-06 11:18:00 +0100

    Mo 6. Mär 13:20:36 CET 2023

---
+++ application/xdg/antiquacrm.1.cmake
@@ -0,0 +1,30 @@
+.IX Title "@PROJECTNAME@ 1"
+.TH @PROJECTNAME@ 1 "@ANTIQUACRM_VERSION_STRING@" "@PROJECTNAME@"
+.SH NAME
+@PROJECTNAME@ \- A graphical interface for managing antiquarian books.
+.SH SYNOPSIS
+.B @PROJECTNAME@
+.SH DESCRIPTION
+@DISPLAYNAME@ is a program for managing antiquarian books.
+It offers item inventory management and a number of online interfaces for simplified data management for the individual service providers.
+
+.IP "\fBFeatures\fR"
+.nf
+\[bu] Customer Address Book,
+\[bu] Book, prints+engravings and CD+Vinyl management,
+\[bu] Job/Order Management, (Depends on the server application!)
+\[bu] Remote office via SSL/TLS,
+\[bu] Pictures management
+\[bu] and Statistics.
+.fi
+.PP
+
+.SH NOTE
+The client application does not make remote requests to the service providers!
+The server application does this by means of an interval and inserts the orders into the database.
+
+.SH HOMEPAGE
+@HOMEPAGEURL@
+
+.SH AUTHOR
+Juergen Heinemann <nospam(AT)hjcms(DOT)de>.
