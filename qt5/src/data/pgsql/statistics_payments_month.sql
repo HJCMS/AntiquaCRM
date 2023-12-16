206724885609dee799c67c77e175e18ae3002cf0
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2023-12-16 03:13:00 +0100

    Sa 16. Dez 03:33:22 CET 2023

---
+++ qt5/src/data/pgsql/statistics_payments_month.sql
@@ -0,0 +1,9 @@
+-- @brief Statistics payments per month
+-- @file statistics_payments_month.sql
+SELECT
+  DATE(o_delivered) as day,
+  EXTRACT(EPOCH FROM DATE (o_delivered)) AS sepoch,
+  count(o_id) AS counts
+FROM inventory_orders
+WHERE date_part('year', o_delivered) <= date_part('year', CURRENT_DATE)
+GROUP BY day ORDER BY day;
--- qt5/src/data/pgsql/statistics_payments_month_in_year.sql
+++
@@ -1,5 +0,0 @@
--- @brief Statistics Payment of  the Year
--- @file statistics_payments_month_in_year.sql
-SELECT DATE(o_delivered) as day, EXTRACT(EPOCH FROM DATE (o_delivered)) AS sepoch, count(o_id) AS counts
-FROM inventory_orders WHERE date_part('year', o_delivered) <= date_part('year', CURRENT_DATE)
-GROUP BY day ORDER BY day;
