32ac25f340da142f5ea5c5a6f03bc587b6c67a2d
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2024-07-04 04:14:00 +0200

    Do 4. Jul 05:48:56 CEST 2024

--- qt6/src/tabs/sellers/purchaseactionbar.cpp
+++ qt6/src/tabs/sellers/purchaseactionbar.cpp
@@ -3,27 +3,29 @@
 
 #include "purchaseactionbar.h"
 
-PurchaseActionBar::PurchaseActionBar(QWidget *parent) : QStatusBar{parent} {
+PurchaseActionBar::PurchaseActionBar(QWidget* parent) : QStatusBar{parent} {
   setSizeGripEnabled(false);
   setContentsMargins(5, 0, 5, 0);
 
   btn_customer = new QPushButton(tr("View Customer"), this);
-  btn_customer->setIcon(AntiquaCRM::antiquaIcon("user-group"));
+  btn_customer->setIcon(AntiquaCRM::antiquaIcon("system-users"));
+  btn_customer->setToolTip(tr("Opens Customer in tab „Customer“."));
   addPermanentWidget(btn_customer);
 
   btn_provider = new QPushButton(tr("Provider Actions"), this);
-  btn_provider->setIcon(AntiquaCRM::antiquaIcon("network"));
+  btn_provider->setIcon(AntiquaCRM::antiquaIcon("preferences-system"));
+  btn_provider->setToolTip(tr("Opens Provider actions dialog."));
   addPermanentWidget(btn_provider);
 
   btn_create = new QPushButton(tr("Create Order"), this);
   btn_create->setIcon(AntiquaCRM::antiquaIcon("action-add"));
+  btn_create->setToolTip(tr("Starts a new assignment in tab „Orders“."));
   btn_create->setEnabled(false);
   addPermanentWidget(btn_create);
 
   btn_check = new QPushButton(tr("Check Articles"), this);
   btn_check->setIcon(AntiquaCRM::antiquaIcon("view-search"));
-  QString info = tr("Create a search query to see if all items are available.");
-  btn_check->setToolTip(info);
+  btn_check->setToolTip(tr("Availability search for all items."));
   addPermanentWidget(btn_check);
 
   connect(btn_customer, SIGNAL(clicked()), SIGNAL(sendViewCustomer()));
