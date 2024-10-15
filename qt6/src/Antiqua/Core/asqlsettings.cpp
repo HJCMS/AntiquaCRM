0d5e568e7f1b8bd09ed0345c548deeb4bc255d07
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2024-10-15 11:26:00 +0200

    Di 15. Okt 11:45:53 CEST 2024

--- qt6/src/Antiqua/Core/asqlsettings.cpp
+++ qt6/src/Antiqua/Core/asqlsettings.cpp
@@ -7,10 +7,12 @@
 
 #include <QDebug>
 #include <QDir>
+#include <QFileInfo>
 
-namespace AntiquaCRM {
+namespace AntiquaCRM
+{
 
-ASqlSettings::ASqlSettings(QObject *parent, const QString &profile)
+ASqlSettings::ASqlSettings(QObject* parent, const QString& profile)
     : ASettings(parent), connection_profile{profile} {
   setObjectName("antiquacrm_sqlsettings");
   if (profile.isEmpty())
@@ -24,7 +26,7 @@ const QString ASqlSettings::groupPath() {
   return group;
 }
 
-const QByteArray ASqlSettings::toRealm(const QString &pass) {
+const QByteArray ASqlSettings::toRealm(const QString& pass) {
   if (pass.isEmpty())
     return QByteArray();
 
@@ -32,7 +34,7 @@ const QByteArray ASqlSettings::toRealm(const QString &pass) {
   return _o.toBase64();
 }
 
-const QString ASqlSettings::fromRealm(const QByteArray &array) {
+const QString ASqlSettings::fromRealm(const QByteArray& array) {
   QString _o;
   QByteArray buf = QByteArray::fromBase64(array, QByteArray::Base64UrlEncoding);
   return _o.fromLocal8Bit(buf);
@@ -43,6 +45,7 @@ const QString ASqlSettings::connectionName() {
 }
 
 const AntiquaCRM::ASqlProfile ASqlSettings::connectionProfile() {
+  QByteArray _arr;
   ASqlProfile _cp(connection_profile);
   _cp.setDatabaseName(getParam("pg_database").toString());
   _cp.setHostname(getParam("pg_hostname").toString());
@@ -53,13 +56,18 @@ const AntiquaCRM::ASqlProfile ASqlSettings::connectionProfile() {
   _cp.setEnableSSL(getParam("pg_ssl").toBool());
   _cp.setSslIssuer(getParam("ssl_CA").toString());
   _cp.setSslCommonName(getParam("ssl_CN").toString());
-  _cp.setSslBundle(getParam("ssl_bundle").toString());
   _cp.setSslMode(getParam("ssl_mode").toString());
-  _cp.setSslRootCert(getParam("ssl_root_cert").toString());
+
+  _arr = getParam("ssl_bundle").toString().trimmed().toUtf8();
+  _cp.setSslBundle(QString::fromUtf8(QByteArray::fromPercentEncoding(_arr)));
+
+  _arr = getParam("ssl_root_cert").toString().trimmed().toUtf8();
+  _cp.setSslRootCert(QString::fromUtf8(QByteArray::fromPercentEncoding(_arr)));
+
   return _cp;
 }
 
-void ASqlSettings::setProfile(const QString &name) {
+void ASqlSettings::setProfile(const QString& name) {
   connection_profile = name.trimmed();
   setValue("database_profile", connection_profile);
 }
@@ -76,7 +84,7 @@ const QStringList ASqlSettings::profiles() {
   return l;
 }
 
-void ASqlSettings::setParam(const QString &key, const QVariant &value) {
+void ASqlSettings::setParam(const QString& key, const QVariant& value) {
   beginGroup(groupPath());
   if (key.contains("password", Qt::CaseInsensitive)) {
     QByteArray buffer = toRealm(value.toString());
@@ -87,7 +95,7 @@ void ASqlSettings::setParam(const QString &key, const QVariant &value) {
   endGroup();
 }
 
-const QVariant ASqlSettings::getParam(const QString &key) {
+const QVariant ASqlSettings::getParam(const QString& key) {
   QVariant out;
   beginGroup(groupPath());
   if (key.contains("password", Qt::CaseInsensitive))
