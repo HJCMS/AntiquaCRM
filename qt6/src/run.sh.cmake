e5e006b2856057201e2bafac11752e3bba7d7c23
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2024-12-25 14:59:00 +0100

    Mi 25. Dez 15:44:19 CET 2024

--- qt6/src/run.sh.cmake
+++ qt6/src/run.sh.cmake
@@ -1,16 +1,46 @@
 #!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8
+##################################################################################
+# This file is part of the HJCMS Project
+#
+# Copyright (C) Juergen Heinemann http://www.hjcms.de, (C) 2007-2025
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public License
+# along with this library; see the file COPYING.LIB.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+##################################################################################
 
-function _childs() {
-  declare -a _list;
-  for i in `find ./Antiqua/ -mindepth 1 -type l -name "*.so" -exec dirname "{}" \;` ; do
-    if test -n "$_list" ; then
-      _list="$_list:$i";
-    else
-      _list="$i";
-    fi
-  done
-  echo $_list
+set +x
+
+test -d ${CMAKE_CURRENT_BINARY_DIR} || exit 1
+
+cd ${CMAKE_CURRENT_BINARY_DIR}
+
+function _libdirs() {
+  find ./Antiqua/ -mindepth 1 -type l \
+     -name "lib*.so" -exec dirname "{}" \; | tr '\012' ':'
 }
 
-export PATH=${CMAKE_PREFIX_PATH}/bin:$(_childs):./plugins:$PATH
-${CMAKE_CURRENT_BINARY_DIR}/${PROJECTNAME}
+export PATH=${CMAKE_PREFIX_PATH}/bin:./plugins:$PATH
+
+if test -n "$LD_LIBRARY_PATH" ; then
+  export LD_LIBRARY_PATH=$(_libdirs).:$LD_LIBRARY_PATH
+else
+  export LD_LIBRARY_PATH=$(_libdirs).
+fi
+
+exec ./${PROJECTNAME} $@
+
+##EOF
